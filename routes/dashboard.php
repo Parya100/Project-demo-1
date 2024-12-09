b9061ed9f8648c8777e67c37655550f73522dca2
Parya <parya3252@gamil.com>
2024-12-09 17:04:56 +0530

    routes

---
+++ routes/dashboard.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>online voting system</title>
+    </head>
+    <body>
+        <button>Back</button>
+        <button>logout</button>
+        <h1>Online voting system</h1>
+        <hr>
+        <div id="Profile"></div>
+        <div id="Group"></div>
+    </body>
+</html>
